94dabf0883f3a7cbd17ff7262fe4b90b1e4792cd
mhmarcinko <marcinko@udel.edu>
2022-07-15 14:31:29 -0400

    R code for simple stats

---
+++ 2_Sample_TTest.R
@@ -0,0 +1,3 @@
+t.test(QuechuaYoungStatus$Response,QuechuaOldStatus$Response, alternative = "two.sided", var.equal = FALSE)
+
+#This conducts a simple 2 sided test for means between QuechuaYoungStatus and QuechuaOldStatus, don’t forget I need to filter data first! Can sub in any filtered data I want
---
+++ Basic_Data_Filtering.R
@@ -0,0 +1,3 @@
+QuechuaYoungStatus= filter (Data,Language.of.voice=='Que'& Age.of.participant == 'Young' & Question.group == 'Status')
+
+#this shows how to filter data. Here, I’ve named it QuechuaYoungStatus, change whatever variables you want, useful to compare means, for ttests, etc
